3d4f91f0f8b8967d5661a8362ec3dd0502874a63
Hugo Ferreira Da Silva <81571386+hugoleao02@users.noreply.github.com>
2022-06-06 03:19:04 -0300

    Update trabalho.cpp

--- trabalho.cpp
+++ trabalho.cpp
@@ -23,16 +23,17 @@ typedef struct client {
      cout<<"Codigo: "<< code << ","<< " Nome: " << name << "," << " Ativo: "
     << (active  ? "Sim" : "Nao" ) << ";"<<endl;
     }
+
 }Client;
 
 typedef struct fuel{
     int code;
     string name;
-    float price;
+    double price;
     Date changeDatePrice;
 
     void print(){
-        cout<<"Codigo: " << code << "," << " Nome: " << name << "," << " Preco: " << price << ","<<
+        cout<<"Codigo: " << code << ",\n" << " Nome: " << name << "," << " Preco: " << price << ",\n"<<
         " Data de alteracao: "<< changeDatePrice.day <<"/"<< changeDatePrice.month <<"/" <<
          changeDatePrice.year << ";"<<endl;
     }
@@ -45,14 +46,12 @@ typedef struct sales{
     double theAmount;
     double literValue;
     double total;
-    string responsible;
     Date date;
 
     void print(){
         cout << "Data: " << date.day <<"/"<< date.month <<"/" << date.year<< ", Codigo do cliente: "
         << cliente.code << ", Codigo do combustivel: "<< combustivel.code<< ", Quantidade: " << theAmount
-        << ", Valor do litro: " << literValue << ", Total: "<< total << ", Reponsavel: "<< responsible
-        << ", Pago: " << (paidOut ? "Sim" : "Nao") << ";"<< endl;
+        << ", Valor do litro: " << literValue << ", Total: "<< total <<", Pago: " << (paidOut ? "Sim" : "Nao") << ";"<< endl;
         }
 }Sales;
 
@@ -60,6 +59,7 @@ void init(map<int,Client>& c, map<int,Fuel>& f);
 void listFuel(map<int,Fuel>&fuel);
 void listClient(map<int,Client>&Client);
 void listSales(map<int,Sales>&sales);
+
 
 int randomId()
 {
@@ -80,10 +80,10 @@ void cadastrarUsuario(map<int, Client>&client)
     cout<<"-------------Cadastrar usuário---------------\n";
     cout<<"Digite o nome do cliente\n";
     cin>> nome;
-    cout<<"Cliente ativo(true or false)?\n";
+    cout<<"Cliente ativo(0= nao, 1= sim)?\n";
     cin>> ativo;
 
-    int id = 1;
+    int id = 8;
 
     novo.code = id++;
     novo.name = nome;
@@ -91,45 +91,10 @@ void cadastrarUsuario(map<int, Client>&client)
     client[novo.code] = novo;
 }
 
-void cadastrarVenda(map<int,Sales>&sales)
-{
-    system("clear||cls");
-    int idCliente;
-    int combustivel;
-    float litros;
-    bool pago;
-    Sales venda;
-    int dia,mes,ano;
-
-
-
-    cout<<"-------------Cadastrar venda---------------\n";
-    cout << "Digite o id do Cliente:\n";
-    cin >> idCliente;
-    cout << "Digite o Codigo do Produto:\n";
-    cin >> combustivel;
-    cout << "Digite quantidade de litros abastecidos:\n";
-    cin >> litros;
-    cout << "Data da compra(dia,mes,ano):\n";
-    cin >> dia;
-    cin >> mes;
-    cin >> ano;
-    cout << "Foi pago?: (0= nao, 1= sim)\n";
-    cin >> pago;
-
-    venda.cliente.code = idCliente;
-    venda.combustivel.code = combustivel;
-    venda.paidOut = pago;
-    venda.date.day = dia;
-    venda.date.month = mes;
-    venda.date.year = ano;
-
-
-    sales[idCliente] = venda;
-}
-
+void cadastrarVenda(map<int,Sales>&sales);
 
 int main() {
+
     map<int,Client>client;
     map<int,Fuel>fuel;
     map<int,Sales>sales;
@@ -137,8 +102,8 @@ int main() {
     int opcao;
     bool sair = 1;
 
-    do{
-
+    while(sair = 1){
+    system("clear||cls");
     int opcao;
     cout <<"-------------------MENU----------------------\n";
     cout <<"1.Cadastrar usuario\n";
@@ -169,75 +134,152 @@ int main() {
             listFuel(fuel);
             break;
     }
-    }while(sair = 1);
 
+};
 
 }
 
 void listClient(map<int,Client>&client){
+    system("clear||cls");
     cout << "\n  Clientes \n\n";
     for(auto i: client){
         if(i.second.active){
         i.second.print();
         }
     }
+    char voltar;
+    cout<<"Deseja voltar para o menu? (s ou n)\n";
+    cin>> voltar;
+        if(voltar == 's'){
+            main();
+        }
+}
+
+void cadastrarVenda(map<int,Sales>&sales){
+    system("clear||cls");
+    int idCliente;
+    int combustivel;
+    double litros;
+    bool pago;
+    Sales venda;
+    int dia,mes,ano;
+    Fuel fuel;
+
+
+    cout<<"-------------Cadastrar venda---------------\n";
+    cout << "Digite o id do Cliente:\n";
+    cin >> idCliente;
+    cout << "Digite o id do combustivel:\n";
+    cin >> combustivel;
+    cout << "Digite quantidade de litros abastecidos:\n";
+    cin >> litros;
+    cout << "Data da compra(dia,mes,ano):\n";
+    cin >> dia;
+    cin >> mes;
+    cin >> ano;
+    cout << "Foi pago?: (0= nao, 1= sim)\n";
+    cin >> pago;
+
+    venda.cliente.code = idCliente;
+    venda.combustivel.code = combustivel;
+    venda.theAmount = litros;
+    venda.paidOut = pago;
+    venda.literValue = fuel.price;
+    venda.total = fuel.price *litros;
+    venda.date.day = dia;
+    venda.date.month = mes;
+    venda.date.year = ano;
+
+    sales[idCliente] = venda;
 }
 
 
 
 void listFuel(map<int,Fuel>&fuel){
+    system("clear||cls");
     cout << "\n  Combustiveis \n\n";
     for(auto i: fuel){
         i.second.print();
     }
+    char voltar;
+    cout<<"Deseja voltar para o menu? (s ou n)\n";
+    cin>> voltar;
+         if(voltar == 's'){
+            main();
+        }
 }
 
 void listSales(map<int,Sales>&sales){
+    system("clear||cls");
     cout << "\n  Vendas \n\n";
     for(auto i: sales){
         i.second.print();
     }
+    char voltar;
+    cout<<"Deseja voltar para o menu? (s ou n)\n";
+    cin>> voltar;
+         if(voltar == 's'){
+            main();
+        }
 }
 
 void init(map<int,Client>& c,map<int,Fuel>& f){
     Client client;
     Fuel fuel;
-    client.code = 2235;
-    c[client.code].code = 45265;
+    client.code = 1;
+    c[client.code].code = 1;
     c[client.code].name = "Hugo Ferreira";
     c[client.code].active = true;
 
-    client.code = 4444;
-    c[client.code].code = 48964;
+    client.code = 2;
+    c[client.code].code = 2;
     c[client.code].name = "Vitor alexandre";
     c[client.code].active = false;
 
-    client.code = 2877;
-    c[client.code].code = 66958;
+    client.code = 4;
+    c[client.code].code = 4;
     c[client.code].name = "Fulano Ciclano";
     c[client.code].active = true;
 
-    fuel.code = 3654;
-    f[fuel.code].code = 568916;
-    f[fuel.code].name = "Diesel s10";
-    f[fuel.code].price = 5.30;
-    f[fuel.code].changeDatePrice.day = 30;
-    f[fuel.code].changeDatePrice.month = 11;
-    f[fuel.code].changeDatePrice.year = 2022;
+    client.code = 5;
+    c[client.code].code = 5;
+    c[client.code].name = "Beutrano Tetrano";
+    c[client.code].active = true;
 
-    fuel.code = 5584;
-    f[fuel.code].code = 56562;
-    f[fuel.code].name = "Gasolina comum";
-    f[fuel.code].price = 6.49;
-    f[fuel.code].changeDatePrice.day = 23;
-    f[fuel.code].changeDatePrice.month = 6;
-    f[fuel.code].changeDatePrice.year = 2021;
+    client.code = 6;
+    c[client.code].code = 6;
+    c[client.code].name = "Caio Ciclano";
+    c[client.code].active = true;
 
-    fuel.code = 2987;
-    f[fuel.code].code = 65631;
-    f[fuel.code].name = "Etanol comum";
-    f[fuel.code].price = 7.49;
-    f[fuel.code].changeDatePrice.day = 02;
-    f[fuel.code].changeDatePrice.year = 2022;
+    client.code = 7;
+    c[client.code].code = 7;
+    c[client.code].name = "Arthur Ciclano";
+    c[client.code].active = true;
+
+    fuel.code = 1;
+    fuel.name = "Diesel s10";
+    fuel.price = 5.00;
+    fuel.changeDatePrice.day = 30;
+    fuel.changeDatePrice.month = 11;
+    fuel.changeDatePrice.year = 2022;
+
+    f[fuel.code] = fuel;
+
+    fuel.code = 2;
+    fuel.name = "Gasolina comum";
+    fuel.price = 6.40;
+    fuel.changeDatePrice.day = 23;
+    fuel.changeDatePrice.month = 6;
+    fuel.changeDatePrice.year = 2021;
+
+    f[fuel.code] = fuel;
+
+    fuel.code = 3;
+    fuel.name = "Etanol comum";
+    fuel.price = 7.20;
+    fuel.changeDatePrice.day = 02;
+    fuel.changeDatePrice.year = 2022;
+
+     f[fuel.code] = fuel;
 }
 
